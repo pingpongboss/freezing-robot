846828b6ad8ac9cb3562ec1085dfcbdc7c41bf73
Mark Wei <markwei@gmail.com>
2012-04-29 08:28:03 -0700

    forgot to save fb

--- helper.js
+++ helper.js
@@ -19,13 +19,15 @@ function fbPostMessage(msg) {
 	});
 }
 
-function fbPostComment(post_id, msg, req) {
-	req.facebook.post(
-		post_id + '/comments',
-		{message: msg},
-		function (data) {
-			console.log('fbPostComment: ' + data);
-		});
+function fbPostComment(post_id, msg) {
+	facebook(function (facebook) {
+		facebook.post(
+			post_id + '/comments',
+			{message: msg},
+			function (data) {
+				console.log('fbPostComment: ' + data);
+			});
+	});
 }
 
 function facebook(callback) {
