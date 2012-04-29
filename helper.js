5542217cc16760812caa6c17a315a26616bc8773
sashaparfenov <sasha@mythictechnologies.com>
2012-04-29 03:48:36 -0700

    adding comment post

--- helper.js
+++ helper.js
@@ -1,13 +1,22 @@
 // TODO: get rid of req, Jamin
 function fbPostMessage(msg, req) {
 	req.facebook.post(
-		'/me/feed'
-		,{
-			message: msg
-		}
-		,function (data) {
+		'/me/feed',
+		{message: msg},
+		function (data) {
 			console.log('fbPostMessage: ' + data);
 		});
 }
 
 exports.fbPostMessage = fbPostMessage;
+
+function fbPostComment(post, msg, req) {
+	req.facebook.post(
+		'/me/feed',
+		{message: msg},
+		function (data) {
+			console.log('fbPostComment: ' + data);
+		});
+}
+
+exports.fbPostComment = fbPostComment;
